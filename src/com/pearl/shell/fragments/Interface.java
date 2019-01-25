1d9790ea11c1b0a39ebc66abdfa8807c34a48aac
Sagar Rokade <Sagarrokade006@gmail.com>
2019-01-25 20:35:57 +0000

    Misc changes Powermenu, LTE toggle, Hold Settings icon to open shell
    Signed-off-by: Dixzz <vishusatyam36@gmail.com>

---
+++ src/com/pearl/shell/fragments/Interface.java
@@ -0,0 +1,63 @@
+package com.pearl.shell.fragments;
+
+import com.android.internal.logging.nano.MetricsProto;
+import android.os.Bundle;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.UserHandle;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import java.util.Locale;
+import android.text.TextUtils;
+import android.view.View;
+import java.util.List;
+import java.util.ArrayList;
+public class Interface extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private ListPreference mQSTileStyle;
+    private static final String QS_TILE_STYLE = "qs_tile_style";
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.inter);
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        // set qs tile style
+        mQSTileStyle = (ListPreference) findPreference(QS_TILE_STYLE);
+        int style = Settings.System.getInt(resolver,
+                Settings.System.QS_TILE_STYLE, 0);
+        mQSTileStyle.setValue(String.valueOf(style));
+        mQSTileStyle.setSummary(mQSTileStyle.getEntry());
+        mQSTileStyle.setOnPreferenceChangeListener(this);
+        }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+       if (preference == mQSTileStyle) {
+            int style = Integer.valueOf((String) newValue);
+            int index = mQSTileStyle.findIndexOfValue((String) newValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.QS_TILE_STYLE, style);
+            mQSTileStyle.setSummary(mQSTileStyle.getEntries()[index]);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.SHELL;
+    }
+}
